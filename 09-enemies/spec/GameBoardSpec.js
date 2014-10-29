d7ef889fd993f082bcf51385a31f6acc6c46a41d
balarcon <balarcon@kappa26.aulas.gsyc.es>
2014-10-29 11:44:25 +0100

    inicializacion 07

--- 09-enemies/spec/GameBoardSpec.js
+++ 09-enemies/spec/GameBoardSpec.js
@@ -56,3 +56,181 @@
     colisionado con objetos de cierto tipo, no con todos los objetos.
 
 */
+describe("Clase GameBoard", function(){
+
+  var canvas, ctx;
+
+  beforeEach(function(){
+  loadFixtures('index.html');
+
+  canvas = $('#game')[0];
+  expect(canvas).toExist();
+
+  ctx = canvas.getContext('2d');
+  expect(ctx).toBeDefined();
+  
+  oldGame = Game;
+    });
+
+    afterEach(function(){
+  Game = oldGame;
+    }); 
+  it("Gameboard.add", function() {
+
+    var gameBoard = new GameBoard();
+
+    //espero que lo que devuelva sea el objeto introducido
+    expect(gameBoard.add(1)).toEqual(1);
+    expect(gameBoard.add(2)).toEqual(2);
+
+    //espero que se haya añadido el objeto introducido
+    expect(gameBoard.objects[0]).toEqual[1];
+    expect(gameBoard.objects[1]).toEqual[2];
+  });
+
+  it("Gameboard.remove", function(){
+    var gameBoard = new GameBoard();
+    //comprubebo que se llama a resetRemoved
+    spyOn(gameBoard, "resetRemoved").andCallThrough();
+    gameBoard.resetRemoved();
+    expect(gameBoard.resetRemoved).toHaveBeenCalled();
+    //compruebo que elimina
+    gameBoard.add(1);
+    gameBoard.add(8);
+    gameBoard.remove(8);
+    gameBoard.remove(1);
+    expect(gameBoard.removed[0]).toEqual(8);
+    expect(gameBoard.removed[1]).toEqual(1);
+  });
+
+  it("Gameboard.resetRemoved", function(){
+    var gameBoard = new GameBoard();
+    expect(gameBoard.resetRemoved()).toEqual();
+  });
+
+  it("Gameboard.finalizeRemoved",function(){
+    var gameBoard = new GameBoard();
+
+    //comprubebo que se llama a resetRemoved
+    spyOn(gameBoard, "resetRemoved").andCallThrough();
+    gameBoard.resetRemoved();
+    expect(gameBoard.resetRemoved).toHaveBeenCalled();
+
+    gameBoard.add(1);
+    gameBoard.add(8);
+    expect(gameBoard.objects[0]).toEqual(1);
+    expect(gameBoard.objects[1]).toEqual(8);
+    gameBoard.remove(8);
+    expect(gameBoard.removed[0]).toEqual(8);
+    expect(gameBoard.objects[0]).toEqual(1);
+    expect(gameBoard.objects[1]).toEqual(8);
+
+    gameBoard.finalizeRemoved();
+    expect(gameBoard.removed[0]).toEqual(8);
+    expect(gameBoard.objects[0]).toEqual(1);
+    expect(gameBoard.objects[1]).toEqual(undefined);
+
+    gameBoard.remove(1);
+    expect(gameBoard.removed[1]).toEqual(1);
+
+    gameBoard.finalizeRemoved();
+    expect(gameBoard.objects[0]).toEqual(undefined);
+    expect(gameBoard.objects[1]).toEqual(undefined);
+  });
+
+  it("Gameboard.iterate",function(){
+    var gameBoard = new GameBoard();
+    //genero dummys con la función que necesito
+    var dummy1 ={
+      funcion: function(){}
+    };
+    var dummy2 ={
+      funcion: function(){}
+    };
+
+    //espio su función
+    spyOn(dummy1,"funcion");
+    spyOn(dummy2,"funcion");
+
+    //los añado al gameboard
+    gameBoard.add(dummy1);
+    gameBoard.add(dummy2);
+    
+    //itero sobre ellos para asegurarme que lo que hacen es llamar a su función con el parámetro que se le ha pasado
+    gameBoard.iterate('funcion', 'hola');
+    expect(dummy1.funcion).toHaveBeenCalledWith('hola');
+    expect(dummy2.funcion).toHaveBeenCalledWith('hola');
+
+  });
+
+    
+
+  it("Gameboard.detect",function(){
+    var gameBoard = new GameBoard();
+    gameBoard.objects = [{att1:1},{att2:2}];
+
+    //funcion verdadero si att1 es 1
+    var func1 = function(){
+      return this.att1==1;
+    };
+
+    //funcion verdadero si att2 es 1
+    var func2 = function(){
+      return this.att2==1;
+    };
+
+    expect(gameBoard.detect(func1)).toBe(gameBoard.objects[0]);
+    expect(gameBoard.detect(func2)).toBe(false);
+  });
+
+  it("Gameboard.step",function(){
+    var gameBoard = new GameBoard();
+    var dt = 30 / 1000;
+
+    spyOn(gameBoard, "resetRemoved");
+    spyOn(gameBoard, "iterate");
+    spyOn(gameBoard, "finalizeRemoved");
+
+    gameBoard.step(dt);
+
+    expect(gameBoard.resetRemoved).toHaveBeenCalled;
+    expect(gameBoard.iterate).toHaveBeenCalledWith('step',dt);
+    expect(gameBoard.finalizeRemoved).toHaveBeenCalled;
+  });
+
+  it("Gameboard.draw",function(){
+    var gameBoard = new GameBoard();
+    spyOn(gameBoard, "iterate");
+    
+    gameBoard.draw(ctx);
+    expect(gameBoard.iterate).toHaveBeenCalledWith('draw',ctx);
+  });
+
+  it("Gameboard.overlap",function(){
+    var gameBoard = new GameBoard();
+    //objeto base
+    var base = {x: 0, y:0, h:20, w:20};
+    var sol = {x: 5, y:5, h:3, w:3};
+    var nosol = {x: 21, y:21, h:3, w:3};
+    //solape
+    expect(gameBoard.overlap(base, sol)).toBeTruthy();
+    //no solape
+    expect(gameBoard.overlap(base, nosol)).toBeFalsy();
+  });
+
+  it("Gameboard.collide",function(){
+    var gameBoard = new GameBoard();
+    var base = {x:0, y:0, h:20, w:20, type:0};
+    var col = {x:5, y:5, h:3, w:3, type:1};
+    var nocol = {x:21, y:21 , h:3 , w:3, type:2};
+
+    gameBoard.add(base);
+    gameBoard.add(col);
+    gameBoard.add(nocol);
+
+    expect(gameBoard.collide(base,col.type)).toBe(col);
+    expect(gameBoard.collide(base,nocol.type)).toBeFalsy;
+  
+  });
+});
+
