22689d82cf13f226dcb32e32e91d5452b29da088
balarcon <beatriz.alarcon@icloud.com>
2014-11-03 13:25:15 +0100

    modificado

--- 10-refactor/spec/PlayerMissileSpec.js
+++ 10-refactor/spec/PlayerMissileSpec.js
@@ -27,3 +27,74 @@
     la clase en el prototipo
 
 */
+describe("Clase PlayerMissile", function(){
+
+
+var canvas, ctx;
+
+  beforeEach(function(){
+    loadFixtures('index.html');
+
+    canvas = $('#game')[0];
+    expect(canvas).toExist();
+
+    ctx = canvas.getContext('2d');
+    expect(ctx).toBeDefined();
+
+    oldGame = Game;
+  });
+
+  afterEach(function(){
+    Game = oldGame;
+  }); 
+
+  it("PlayerMissile", function(){
+    //preparo el SpriteSheet necesario
+    SpriteSheet.map = {
+      missile: {w:2, h:10} 
+    };
+    //compruebo para los parámetros concretos, es más util hacerlo más genérico
+    var playerMissile = new PlayerMissile(10,10);
+    expect(playerMissile.w).toBe(2);
+    expect(playerMissile.h).toBe(10);
+    expect(playerMissile.x).toBe(10+2/2);
+    expect(playerMissile.y).toBe(10+10);
+    expect(playerMissile.vy).toBe(-700);
+  });
+
+  it("PlayerMissile.step",function(){
+    SpriteSheet.map = {
+      missile: {w:2, h:10} 
+    };
+    var playerMissile = new PlayerMissile(10,10);
+    var gameBoard = {remove: function(x){}};
+    spyOn(gameBoard, "remove");
+
+    playerMissile.board = gameBoard;
+
+    //0<-10 =>no llama a remove
+    playerMissile.step(0);
+    expect(gameBoard.remove).not.toHaveBeenCalled();
+    //(-700*1)<-10 =>no llama a remove
+    playerMissile.step(1);
+    expect(gameBoard.remove).toHaveBeenCalledWith(playerMissile);
+
+  });
+
+  it("PlayerMissile.draw",function(){
+    SpriteSheet= {
+      draw: function(a, b, c, d){}
+    };
+    SpriteSheet.map = {
+      missile: {w:2, h:10} 
+    };
+    ctx ={};
+    var playerMissile = new PlayerMissile(10,20);
+    spyOn(SpriteSheet, "draw");
+
+    playerMissile.draw(ctx);
+    expect(SpriteSheet.draw).toHaveBeenCalledWith(ctx,'missile',playerMissile.x, playerMissile.y,0)
+  });
+
+
+});
