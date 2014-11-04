b7280db4ca5b8fc3c11d75aed71631979edd6915
balarcon <beatriz.alarcon@icloud.com>
2014-11-04 12:53:32 +0100

    specs prototipos anteriores

--- 12-levels/spec/CollisionsSpec.js
+++ 12-levels/spec/CollisionsSpec.js
@@ -6,9 +6,10 @@
   varios tipos de sprites:
   
   - Los misiles tienen ahora una nueva propiedad: el daño (damage) que
-    producen cuando colisionan con una nave enemiga. Cuando un misil
-    colisione con una nave enemiga le infligirá un daño de cierta
-    cuantía a la nave enemiga con la que impacta, y desaparecerá.
+    infligen a una nave enemiga cuando colisionan con ella. Cuando un
+    misil colisione con una nave enemiga le infligirá un daño de
+    cierta cuantía (damage) a la nave enemiga con la que impacta, y
+    desaparecerá.
 
   - Las naves enemigas tienen ahora una nueva propiedad: su salud
     (health).  El daño ocasionado a una nave enemiga por un misil hará
@@ -46,35 +47,142 @@
 
   No interesa comprobar si se colisiona con cualquier otro objeto,
   sino sólo con los de ciertos tipos. El misil tiene que comprobar si
-  colisiona con enemigos. El enemigo tiene que comprobar si colisiona
-  con la nave del jugador. Para ello cada sprite tiene un tipo y
-  cuando se comprueba si un sprite ha colisionado con otros, se pasa
-  como segundo argumento a collide() el tipo de sprites con los que se
-  quiere ver si ha colisionado el objeto que se pasa como primer
-  argumento.
+  colisiona con naves enemigas. Por otro lado, tras moverse una nave
+  enemiga, ésta tiene que comprobar si colisiona con la nave del
+  jugador. Para ello cada sprite tiene un tipo y cuando se comprueba
+  si un sprite ha colisionado con otros, se pasa como segundo
+  argumento a collide() el tipo de sprites con los que se quiere ver
+  si ha colisionado el objeto que se pasa como primer argumento.
 
-  Cuando un objeto detecta que ha colisionado con otro llama al método
-  hit() del objeto con el que ha colisionado. El misil cuando llama a
-  hit() de una nave enemiga pasa como parámetro el daño que provoca
-  para que la nave enemiga pueda calcular la reducción de salud que
-  conlleva la colisión.
+  Cuando un objeto detecta que ha colisionado con otro, llama al
+  método hit() del objeto con el que ha colisionado. 
 
 
-  Efectos de las colisiones:
+  Efectos de las colisiones de un misil con una nave enemiga:
 
-  Cuando una nave enemiga recibe la llamada .hit() realizada por un
-  misil que ha detectado la colisión, recalcula su salud reduciéndola
-  en tantas unidades como el daño del misil indique, y si su salud
-  llega a 0 desaparece del tablero de juegos, produciéndose en su
-  lugar la animación de una explosión.
+    Cuando el misil llama al método hit() de una nave enemiga, pasa
+    como parámetro el daño que provoca para que la nave enemiga pueda
+    calcular la reducción de salud que conlleva la colisión. Cuando
+    una nave enemiga recibe una llamada a su método .hit() realizada
+    por un misil que ha detectado la colisión, la nave enemiga
+    recalcula su salud reduciéndola en tantas unidades como el daño
+    del misil indique, y si su salud llega a 0 desaparece del tablero
+    de juegos, produciéndose en su lugar la animación de una
+    explosión.
 
-  Cuando la nave del jugador recibe la llamada .hit() realizada por
-  una nave enemiga que ha detectado la colisión, desaparece.
+    El misil, tras informar llamando al métod hit() de la nave enemiga
+    con la que ha detectado colisión, desaparece.
 
-  El misil, tras informar llamando al métod hit() de la nave enemiga
-  con la que ha detectado colisión, desaparece.
 
-  La nave enemiga, tras informar llamando a hit() de la nave del
-  jugador, desaparece.
+  Efectos de las colisiones de una nave enemiga con la nave del jugador:
+
+    Cuando la nave del jugador recibe la llamada .hit() realizada por
+    una nave enemiga que ha detectado la colisión, la nave del jugador
+    desaparece del tablero.
+
+    La nave enemiga, tras informar llamando a hit() de la nave del
+    jugador, desaparece también.
 
 */
+describe("Collision", function(){
+
+
+var canvas, ctx;
+
+  beforeEach(function(){
+    loadFixtures('index.html');
+
+    canvas = $('#game')[0];
+    expect(canvas).toExist();
+
+    ctx = canvas.getContext('2d');
+    expect(ctx).toBeDefined();
+ 
+    oldGame = Game;
+  });
+
+  afterEach(function(){
+    Game = oldGame;
+  }); 
+
+  it("misil colisiona enemigo", function(){
+    SpriteSheet.map ={
+      ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
+      missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
+      enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
+      explosion: { sx: 0, sy: 64, w: 64, h: 64, frames: 12 },
+    };
+    var gameBoard = new GameBoard();
+    basics = { x: 100, y: -50, sprite: 'enemy_purple', B: 100, C: 2 , E: 100};
+    var myenemy = new Enemy(basics);
+    var mymissile = new PlayerMissile(10,20);
+    mymissile.damage = 200;
+
+    gameBoard.add(myenemy);
+    gameBoard.add(mymissile);
+
+    gameBoard.step(100);
+
+    expect(gameBoard.objects.length).toBe(0);   
+
+
+  });
+
+  it("bola de fuego colisiona enemigo",function(){
+    SpriteSheet.map ={
+      ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
+      enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
+      explosion: { sx: 0, sy: 64, w: 64, h: 64, frames: 12 },
+      fireball: {sx: 0, sy: 64, w: 64, h: 64, frames: 1}
+    };
+    var gameBoard = new GameBoard();
+    basics = { x: 100, y: -50, sprite: 'enemy_purple', B: 0, C: 2 , E: 100};
+    var fireBall = new FireBall(100, 100, "dch");
+
+    fireBall.x=100;
+    fireBall.y=200;
+    fireBall.vx=0;
+    fireBall.vy=0;
+    fireBall.w=0;
+    fireBall.h=0;
+   
+
+    var myenemy = new Enemy(basics);
+
+    gameBoard.add(myenemy);
+    gameBoard.add(fireBall);
+    expect(gameBoard.objects.length).toBe(2);
+
+    gameBoard.step(100);
+
+    expect(gameBoard.objects.length).toBe(1);
+    expect(gameBoard.objects[0].sprite).toEqual('fireball');
+    
+
+  });
+
+  it("nave colisiona enemigo", function(){
+    SpriteSheet.map ={
+      ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
+      enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 }
+    };
+    basics = { x: 100, y: 100, sprite: 'enemy_purple', B: 100, C: 2 , E: 100 };
+    var gameBoard = new GameBoard();
+    var myenemy = new Enemy(basics);
+    var myship = new PlayerShip();
+    myship.x=100;
+    myship.y=100;
+   
+    gameBoard.add(myenemy);
+    gameBoard.add(myship);
+
+    expect(gameBoard.objects.length).toBe(2);
+    gameBoard.step(3/1000);
+
+    //al chocar se eliminan los dos objetos
+    expect(gameBoard.objects.length).toBe(0);
+    expect(gameBoard.objects[0]).toEqual(undefined);
+
+  });
+
+});
