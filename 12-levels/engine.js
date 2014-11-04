1cadba26419185bd12d4345ed783f09852533fc4
balarcon <beatriz.alarcon@icloud.com>
2014-11-04 13:33:16 +0100

    underscore

--- 12-levels/engine.js
+++ 12-levels/engine.js
@@ -9,7 +9,8 @@
 // puntuación o el número de vidas.
 
 
-
+//implementación con underscore
+var _;
 
 // Objeto singleton Game: se guarda una unica instancia del
 // constructor anónimo en el objeto Game
@@ -220,21 +221,25 @@ var GameBoard = function() {
     // Iterador que aplica el método funcName a todos los
     // objetos de objects
     this.iterate = function(funcName) {
-	// Convertimos en un array args (1..)
-	var args = Array.prototype.slice.call(arguments,1);
+    // Convertimos en un array args (1..)
+       var args = Array.prototype.slice.call(arguments,1);
 
-	for(var i=0, len=this.objects.length; i<len;i++) {
-	    var obj = this.objects[i];
-	    obj[funcName].apply(obj,args)
-	}
+    /*for(var i=0, len=this.objects.length; i<len;i++) {
+        var obj = this.objects[i];
+        obj[funcName].apply(obj,args)
+    }*/
+
+        _(this.objects).each(function(obj){obj[funcName].apply(obj,args)});
     };
 
     // Devuelve el primer objeto de objects para el que func es true
     this.detect = function(func) {
-	for(var i = 0,val=null, len=this.objects.length; i < len; i++) {
-	    if(func.call(this.objects[i])) return this.objects[i];
-	}
-	return false;
+    /*for(var i = 0,val=null, len=this.objects.length; i < len; i++) {
+        if(func.call(this.objects[i])) return this.objects[i];
+    }
+    return false;*/
+    return _(this.objects).find(function(aux){
+        if(func.call(aux)) return aux;}) || false;
     };
 
     // Cuando Game.loop() llame a step(), hay que llamar al método
